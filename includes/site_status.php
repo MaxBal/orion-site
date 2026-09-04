8d589faba435f93ab2e508301629d886ba94046f
Max Baulin <max.baulin15@gmail.com>
2026-09-04 18:42:16 +0100

    Cache server status 30s in session, reduce UDP timeout to 1s

--- includes/site_status.php
+++ includes/site_status.php
@@ -25,6 +25,14 @@ function orion_server_status_error($key): string
 }
 
 function orion_server_state($pdo) {
+    // Кешируем статус на 30 секунд в сессии
+    $cache_key = 'server_status_cache';
+    $cache_ttl = 30;
+    
+    if (isset($_SESSION[$cache_key]) && ($_SESSION[$cache_key]['time'] ?? 0) > time() - $cache_ttl) {
+        return $_SESSION[$cache_key]['data'];
+    }
+    
     // Реальная проверка сервера по UDP
     $ip = '62.84.175.101';
     $ports = [20016, 20017];
@@ -35,7 +43,7 @@ function orion_server_state($pdo) {
     foreach ($ports as $port) {
         $sock = @socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
         if ($sock) {
-            socket_set_option($sock, SOL_SOCKET, SO_RCVTIMEO, ['sec' => 2, 'usec' => 0]);
+            socket_set_option($sock, SOL_SOCKET, SO_RCVTIMEO, ['sec' => 1, 'usec' => 0]);
             @socket_sendto($sock, "\xff\xff\xff\xff\x54Source Engine Query\x00", 25, 0, $ip, $port);
             $buf = ''; $from = ''; $rport = 0;
             if (@socket_recvfrom($sock, $buf, 4096, 0, $from, $rport)) {
@@ -50,19 +58,17 @@ function orion_server_state($pdo) {
         if ($is_online) break;
     }
     
-    $message = '';
-    if ($is_online) {
-        $message = "Players: $players/$max_players";
-    }
-    
-    return [
+    $result = [
         'status' => $is_online ? 'online' : 'offline',
         'is_online' => $is_online,
-        'message' => $message,
+        'message' => $is_online ? "Players: $players/$max_players" : '',
         'players' => $players,
         'max_players' => $max_players,
         'updated_at' => date('Y-m-d H:i:s'),
     ];
+    
+    $_SESSION[$cache_key] = ['time' => time(), 'data' => $result];
+    return $result;
 }
 
 function orion_set_server_state($pdo, $status, $message = '') {
